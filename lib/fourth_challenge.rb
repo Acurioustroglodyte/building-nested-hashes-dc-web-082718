0499c552307209e716a034daccaf7a53c5258b50
Trevor Jameson <tjameson15@gmail.com>
2018-07-18 17:05:10 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -3,34 +3,34 @@ def fourth_challenge
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero => {name: "Romeo", age: "15", attitude: "alive"},
       :hero_friends => [
         {
           name: "Benvolio",
           age: "17",
-          status: "worried"
+          attitude: "worried"
       },
       {
         name: "Mercutio",
         age: "18",
-        status: "hot-headed"
+        attitude: "hot-headed"
       }
       ]
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine => {name: "Juliet", age: "15", attitude: "alive"},
       :heroine_friends => [
         {
           name: "Steven",
           age: "30",
-          status: "confused"
+          attitude: "confused"
         },
         {
           name: "Nurse",
           age: "44",
-          status: "worried"
+          attitude: "worried"
         }
       ]
    }
